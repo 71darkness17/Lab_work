5df08db4f04cdd17b48666cb9dd1b9f6671a61a5
Владимир Заворохин <75090682+71darkness17@users.noreply.github.com>
2024-11-27 13:17:17 +0300

    Добавлены комментарии к коду sudoku.c

--- sudoku.c
+++ sudoku.c
@@ -15,13 +15,16 @@
 int main(){
     srand(time(NULL));
     int mode;
+    // Очистка консоли и ввод размера судоку
     system("cls");
     puts("4 - 4x4, 9 - 9x9");
     scanf("%d",&mode);
+    // генерация судоку
     int ** sudoku = generate_null_matrix(mode);
     sudoku = generate_sudoku(sudoku,mode);
     puts("\n");
     int hide = create_task(sudoku,mode);
+    // вывод судоку и выбор режима
     system("cls");
     print_sudoku(sudoku,mode);
     puts("1 - brutforce, -1 - game");
@@ -31,12 +34,11 @@ int main(){
     int ** pos = get_positions(sudoku,mode,hide);
     system("cls");
     puts("\n");
+    // начало брутфорса или игры
     if(decision == 1) brutforce(sudoku,mode,hide);
     else game(sudoku,mode,hide);
     puts("\n");
-    // for (int i = 0;i<hide;i++){
-    //     printf("(%d, %d) ",*(*(pos+i)),*(*(pos+i)+1));
-    // }
+    // освобождение выделенного места
     free_matrix(sudoku,mode);
     return 0;
 }
